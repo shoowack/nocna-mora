5b58fe2eff7e33a06d45ab72444214b4bf3fdd23
Ivan Suvak Martinovic <isuvak@gmail.com>
2024-09-22 23:26:38 -0400

    Fix header z-index

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@/components/user-button";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 flex justify-center border-b bg-white">
+    <header className="sticky top-0 z-10 flex justify-center border-b bg-white">
       <Container className="flex h-16 items-center justify-between md:py-6">
         <MainNav />
         <UserButton />
