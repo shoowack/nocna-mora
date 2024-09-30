2e0b87878cfc86b79c47ea8502db01e7e1a10eda
Ivan Suvak Martinovic <isuvak@gmail.com>
2024-09-30 13:49:39 -0400

    Lint

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -40,15 +40,16 @@ export default function PrivacyPolicy() {
             <p>
               <strong>Affiliate</strong> means an entity that controls, is
               controlled by or is under common control with a party, where
-              "control" means ownership of 50% or more of the shares, equity
-              interest or other securities entitled to vote for election of
-              directors or other managing authority.
+              &quot;control&quot; means ownership of 50% or more of the shares,
+              equity interest or other securities entitled to vote for election
+              of directors or other managing authority.
             </p>
           </li>
           <li>
             <p>
-              <strong>Company</strong> (referred to as either "the Company",
-              "We", "Us" or "Our" in this Agreement) refers to Nocna Mora.
+              <strong>Company</strong> (referred to as either &quot;the
+              Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot;
+              in this Agreement) refers to Noćna Mora.
             </p>
           </li>
           <li>
@@ -151,7 +152,7 @@ export default function PrivacyPolicy() {
         <h4>Usage Data</h4>
         <p>Usage Data is collected automatically when using the Service.</p>
         <p>
-          Usage Data may include information such as Your Device's Internet
+          Usage Data may include information such as Your Device&apos;s Internet
           Protocol address (e.g. IP address), browser type, browser version, the
           pages of our Service that You visit, the time and date of Your visit,
           the time spent on those pages, unique device identifiers and other
@@ -185,17 +186,17 @@ export default function PrivacyPolicy() {
         <p>
           If You decide to register through or otherwise grant us access to a
           Third-Party Social Media Service, We may collect Personal data that is
-          already associated with Your Third-Party Social Media Service's
+          already associated with Your Third-Party Social Media Service&apos;s
           account, such as Your name, Your email address, Your activities or
           Your contact list associated with that account.
         </p>
         <p>
           You may also have the option of sharing additional information with
-          the Company through Your Third-Party Social Media Service's account.
-          If You choose to provide such information and Personal Data, during
-          registration or otherwise, You are giving the Company permission to
-          use, share, and store it in a manner consistent with this Privacy
-          Policy.
+          the Company through Your Third-Party Social Media Service&apos;s
+          account. If You choose to provide such information and Personal Data,
+          during registration or otherwise, You are giving the Company
+          permission to use, share, and store it in a manner consistent with
+          this Privacy Policy.
         </p>
         <h4>Tracking Technologies and Cookies</h4>
         <p>
@@ -225,10 +226,10 @@ export default function PrivacyPolicy() {
           </li>
         </ul>
         <p>
-          Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies
-          remain on Your personal computer or mobile device when You go offline,
-          while Session Cookies are deleted as soon as You close Your web
-          browser.
+          Cookies can be &quot;Persistent&quot; or &quot;Session&quot; Cookies.
+          Persistent Cookies remain on Your personal computer or mobile device
+          when You go offline, while Session Cookies are deleted as soon as You
+          close Your web browser.
         </p>
         <p>
           We use both Session and Persistent Cookies for the purposes set out
@@ -310,11 +311,11 @@ export default function PrivacyPolicy() {
             <p>
               <strong>To contact You:</strong> To contact You by email,
               telephone calls, SMS, or other equivalent forms of electronic
-              communication, such as a mobile application's push notifications
-              regarding updates or informative communications related to the
-              functionalities, products or contracted services, including the
-              security updates, when necessary or reasonable for their
-              implementation.
+              communication, such as a mobile application&apos;s push
+              notifications regarding updates or informative communications
+              related to the functionalities, products or contracted services,
+              including the security updates, when necessary or reasonable for
+              their implementation.
             </p>
           </li>
           <li>
@@ -415,12 +416,12 @@ export default function PrivacyPolicy() {
         <h3 className="text-xl font-bold">Transfer of Your Personal Data</h3>
         <p>
           Your information, including Personal Data, is processed at the
-          Company's operating offices and in any other places where the parties
-          involved in the processing are located. It means that this information
-          may be transferred to — and maintained on — computers located outside
-          of Your state, province, country or other governmental jurisdiction
-          where the data protection laws may differ than those from Your
-          jurisdiction.
+          Company&apos;s operating offices and in any other places where the
+          parties involved in the processing are located. It means that this
+          information may be transferred to — and maintained on — computers
+          located outside of Your state, province, country or other governmental
+          jurisdiction where the data protection laws may differ than those from
+          Your jurisdiction.
         </p>
         <p>
           Your consent to this Privacy Policy followed by Your submission of
@@ -492,7 +493,7 @@ export default function PrivacyPolicy() {
           acceptable means to protect Your Personal Data, We cannot guarantee
           its absolute security.
         </p>
-        <h2 className="text-2xl font-bold">Children's Privacy</h2>
+        <h2 className="text-2xl font-bold">Children&apos;s Privacy</h2>
         <p>
           Our Service does not address anyone under the age of 13. We do not
           knowingly collect personally identifiable information from anyone
@@ -505,15 +506,15 @@ export default function PrivacyPolicy() {
         <p>
           If We need to rely on consent as a legal basis for processing Your
           information and Your country requires consent from a parent, We may
-          require Your parent's consent before We collect and use that
+          require Your parent&apos;s consent before We collect and use that
           information.
         </p>
         <h2 className="text-2xl font-bold">Links to Other Websites</h2>
         <p>
           Our Service may contain links to other websites that are not operated
           by Us. If You click on a third party link, You will be directed to
-          that third party's site. We strongly advise You to review the Privacy
-          Policy of every site You visit.
+          that third party&apos;s site. We strongly advise You to review the
+          Privacy Policy of every site You visit.
         </p>
         <p>
           We have no control over and assume no responsibility for the content,
@@ -526,8 +527,8 @@ export default function PrivacyPolicy() {
         </p>
         <p>
           We will let You know via email and/or a prominent notice on Our
-          Service, prior to the change becoming effective and update the "Last
-          updated" date at the top of this Privacy Policy.
+          Service, prior to the change becoming effective and update the
+          &quot;Last updated&quot; date at the top of this Privacy Policy.
         </p>
         <p>
           You are advised to review this Privacy Policy periodically for any
