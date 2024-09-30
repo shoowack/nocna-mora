92e1ea6ca86b65d63cf7abdcaf2640cd4d4a79a1
Ivan Suvak Martinovic <isuvak@gmail.com>
2024-09-30 13:16:01 -0400

    `Privacy policy` page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@/components/container";
+
+export default function PrivacyPolicy() {
+  return (
+    <Container className="text-center">Privacy Policy Placeholder</Container>
+  );
+}
