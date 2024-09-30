1a9dedd8f8492234bcd38ee0d7932925ebaaf75a
Ivan Suvak Martinovic <isuvak@gmail.com>
2024-09-30 18:32:00 -0400

    Add Google auth provider

--- auth.ts
+++ auth.ts
@@ -2,17 +2,17 @@ import NextAuth, { type DefaultSession } from "next-auth";
 import "next-auth/jwt";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-
 import FacebookProvider, {
   FacebookProfile,
 } from "next-auth/providers/facebook";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 import GitHub, { GitHubProfile } from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
 
 const prisma = new PrismaClient();
 
 const config = {
-  theme: { logo: "https://authjs.dev/img/logo-sm.png" },
+  theme: { logo: "/nocna-mora-logo-alt.svg" },
   providers: [
     GitHub({
       profile(profile: GitHubProfile) {
@@ -22,6 +22,17 @@ const config = {
           email: profile.email,
           name: profile.name,
           image: profile.avatar_url,
+        };
+      },
+    }),
+    GoogleProvider({
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub.toString(),
+          role: "user",
+          email: profile.email,
+          name: profile.name,
+          image: profile.picture,
         };
       },
     }),
