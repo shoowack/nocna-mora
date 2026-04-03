e126f2e2a76c7ba3028806f471822eff15528f23
Ivan Suvak Martinovic <isuvak@gmail.com>
2026-04-03 16:06:45 -0300

    Revert "Refactor Umami identify to use data-before-send instead of onReady"
    This reverts commit 67fb72bc87c8020e74537de5e865c11147701e00.

--- src/components/UmamiIdentify.tsx
+++ src/components/UmamiIdentify.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Script from 'next/script'
-import { useEffect } from 'react'
 
 interface Props {
   websiteId: string
@@ -15,26 +14,18 @@ interface Props {
 }
 
 export function UmamiIdentify({ websiteId, user }: Props) {
-  useEffect(() => {
-    if (user) {
-      const { id, ...data } = user
-      ;(window as any).umamiBeforeSend = (_type: string, payload: any) => ({
-        ...payload,
-        id: String(id),
-        data: { ...payload?.data, ...data },
-      })
-    } else {
-      ;(window as any).umamiBeforeSend = undefined
-    }
-  }, [user?.id])
-
   return (
     <Script
       src="https://stats.nocna-mora.com/script.js"
       data-website-id={websiteId}
       data-performance="true"
-      data-before-send="umamiBeforeSend"
       strategy="afterInteractive"
+      onReady={() => {
+        if (user && (window as any).umami?.identify) {
+          const { id, ...data } = user
+          ;(window as any).umami.identify(String(id), data)
+        }
+      }}
     />
   )
 }
