544098df96b35ecafab9219b71f0a9a5a165d8f5
Ivan Suvak Martinovic <isuvak@gmail.com>
2026-04-05 09:25:33 -0300

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
 import './globals.css'
+import { Geist } from "next/font/google";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
